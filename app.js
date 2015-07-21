1ca841bda5e4ec15fa46ed67dc74b8ec727b43a7
thangnv <thangtyt@gmail.com>
2015-07-21 15:25:48 +0700

    upload bt

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
  * Created by thangnv on 7/20/15.
  */
 var app = require('express')();
-
+var qs = require('querystring');
 
 var nunjucks = require('nunjucks');
 var bodyParser = require('body-parser');
@@ -10,15 +10,28 @@ nunjucks.configure('views',{
     autoescape : true,
     express : app
 })
-app.set('view engine','html');
-app.use(bodyParser.urlencoded());
+
+app.use(function (req,res,next) {
+    if (req.method == 'POST') {
+        var content = '';
+        req.on('data', function (data) {
+            content += data;
+        });
+        req.on('end', function () {
+            req.body = qs.parse(content);
+            next();
+        });
+    }else{
+        next();
+    }
+
+});
 app.get('/', function (req,res) {
     res.render('index.html');
 })
 app.post('/', function (req,res) {
-    console.log(req.body);
-    var data = req.body;
-    res.render('hello.html',{data : data});
+    res.json(req.body)
+    //res.render('hello.html',{data : req.body});
 })
 app.listen(2222, function () {
     console.log('Server is running ...');
